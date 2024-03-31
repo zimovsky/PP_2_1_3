e55dd18e19a6cd4ccdfcd1607ada04b4ae148e4f
Sergey Zimovsky <sergey@zimovsky.pro>
2024-03-31 03:16:56 +0300

    Try to please the mentor xD
    - Create a Dog class
    - Make it component
    - Add a @Qualifier to AnimalCage
    - Create a bean from Timer class
    - Wire it to AnimalCage
    - Add a getTimer method
    - Run test

--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -1,13 +1,18 @@
 package app.model;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AnimalsCage {
 
+    @Qualifier("dog")
     @Autowired
     private Animal animal;
+
+    @Autowired
+    private Timer timer;
 
     public void whatAnimalSay() {
         System.out.println("Say:");
@@ -15,5 +20,9 @@ public class AnimalsCage {
         System.out.println("At:");
         System.out.println(new Timer().getTime());
         System.out.println("________________________");
+    }
+
+    public Timer getTimer() {
+        return this.timer;
     }
 }
